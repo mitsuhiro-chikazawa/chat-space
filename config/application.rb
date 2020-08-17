44c0c21174b5cc51258674e95c70b1bfb82b7e0b
chika-tsubaru <k67e5b9fd7zh1@softbank.ne.jp>
2020-08-17 09:47:09 +0900

    implementation management user

--- config/application.rb
+++ config/application.rb
@@ -2,13 +2,10 @@ require_relative 'boot'
 
 require 'rails/all'
 
-# Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module ChatSpace
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
     config.generators do |g|
       g.stylesheets false
@@ -16,5 +13,6 @@ module ChatSpace
       g.helper false
       g.test_framework false
     end
+    config.i18n.default_locale = :ja
   end
 end
