17fa89381d31142b7011cf19dff8dab4a5872599
chika-tsubaru <k67e5b9fd7zh1@softbank.ne.jp>
2020-08-26 21:47:44 +0900

    create_group_edit

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -12,6 +12,7 @@ class GroupsController < ApplicationController
     @group = Group.new(group_params)
     if @group.save
       redirect_to root_path, notice: 'グループを作成しました'
+      # redirect_to group_messages_path(@group)
     else
       render :new
     end
@@ -20,12 +21,13 @@ class GroupsController < ApplicationController
 
   def edit
     @group = Group.find(params[:id])
+    # binding.pry
   end
 
   def update
     @group = Group.find(params[:id])
     if @group.update(group_params)
-      redirect_to root_path, notice: 'グループを更新しました'
+      redirect_to group_messages_path(@group), notice: 'グループを更新しました'
     else
       render :edit
     end
