8bbad3f967d04405d390b031991774baf319a9e7
chika-tsubaru <k67e5b9fd7zh1@softbank.ne.jp>
2020-09-10 20:59:27 +0900

    wip-create-auto-update

--- app/assets/javascripts/autoReload.js
+++ app/assets/javascripts/autoReload.js
@@ -2,7 +2,7 @@ $(function(){
   function buildHTML(message){
     if ( message.image ) {
       let html =
-        `<div class="MessageBox">
+        `<div class="MessageBox" data-message-id=${message.id}>
           <div class="MessageInfo">
             <div class="MessageInfo__userName">
               ${message.user_name}
@@ -12,8 +12,8 @@ $(function(){
             </div>
           </div>
           <div class="Message">
-            <p class="Message__body">
-              ${message.body}
+            <p class="Message__content">
+              ${message.content}
             </p>
             <img class="Message__image" src="${message.image}">
           </div>
@@ -21,7 +21,7 @@ $(function(){
       return html;
     } else {
       let html =
-      `<div class="MessageBox">
+      `<div class="MessageBox" data-message-id=${message.id}>
         <div class="MessageInfo">
           <div class="MessageInfo__userName">
             ${message.user_name}
@@ -40,27 +40,27 @@ $(function(){
     };
   }
 
-  $('.form').on('submit', function(e){
-    e.preventDefault();
-    let formData = new FormData(this);
-    let url = $(this).attr('action');
+  let reloadMessages = function() {
+    let last_message_id = $('.MessageBox:last').data("message-id") || 0;
     $.ajax({
-      url: url,
-      type: "POST",
-      data: formData,
+      url: "api/messages",
+      type: 'get',
       dataType: 'json',
-      processData: false,
-      contentType: false
+      data: {id: last_message_id}
     })
-    .done(function(data){
-      let html = buildHTML(data);
-      $('.chatmain__message--field').append(html);
-      $('form')[0].reset();
-      $('.chatmain__message--field').animate({ scrollTop: $('.chatmain__message--field')[0].scrollHeight});
-      $('.btn').prop('disabled', false);
+    .done(function(messages) {
+      if (messages.length !== 0) {
+        let insertHTML = '';
+        $.each(messages, function(i, message) {
+          insertHTML += buildHTML(message)
+        });
+        $('.chatmain__message--field').append(insertHTML);
+        $('.chatmain__message--field').animate({ scrollTop: $('.chatmain__message--field')[0].scrollHeight});
+      }
     })
     .fail(function() {
-      alert("メッセージ送信に失敗しました");
+      alert('error');
     });
-  });
+  };
+  setInterval(reloadMessages, 7000);
 });
